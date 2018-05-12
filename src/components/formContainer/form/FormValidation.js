69d6af52bc8121e6fa3e7ef6a8f1259a1158b636
walter090 <walter.wu090@gmail.com>
2018-05-12 00:57:26 -0400

    Update form validation
    merge password validation and strength check. Add username validation.

--- src/components/formContainer/form/FormValidation.js
+++ src/components/formContainer/form/FormValidation.js
@@ -1,26 +1,36 @@
-export function emailValidate(value) {
+export function emailValidate(email) {
     const re = /^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i;
-    const valid = re.test(value.toLowerCase());
+    const valid = re.test(email.toLowerCase());
     return {
         valid: valid,
         message: 'Please enter a valid email address.'
     };
 }
 
-export function passwordConfirm(password, confirm) {
-    const valid = (password === confirm || (password === '' || confirm === ''));
-    return {
-        valid: valid,
-        message: 'Passwords do not match.'
-    };
+export function passwordValidate(password) {
+    if (password === '') {
+        return {
+            valid: false,
+            message: 'Password is required.'
+        };
+    } else {
+        return passwordStrength(password);
+    }
 }
 
 export function passwordStrength(password) {
-    const valid = (password.length >= 8);
+    const valid = password.length >= 8;
     return {
         valid: valid,
         message: 'Your password is not strong enough.'
     };
 }
 
-export default {emailValidate, passwordConfirm, passwordStrength};
+export function usernameValidate(username) {
+    return {
+        valid: username !== '',
+        message: 'Username is required.'
+    }
+}
+
+export default {emailValidate, passwordValidate, usernameValidate};
