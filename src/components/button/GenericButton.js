25b8483a64dd03dbba0099780380f0bc8410eebd
walter090 <walter.wu090@gmail.com>
2018-05-12 00:55:50 -0400

    Add prop type check to generic button component

--- src/components/button/GenericButton.js
+++ src/components/button/GenericButton.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class GenericButton extends React.Component {
     render() {
         return (
-            <button onClick={this.props.onClick}>{this.props.text}</button>
+            <button onClick={this.props.onClick}
+                    className='gen-btn'
+                    id={this.props.id}>
+                {this.props.text}
+                </button>
         );
     }
 }
 
 export default GenericButton;
+
+GenericButton.propTypes = {
+    onClick: PropTypes.func,
+    id: PropTypes.string,
+    text: PropTypes.string,
+    className: PropTypes.string
+};
