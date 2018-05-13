b54ff7d8142a5e7cc751e0339cb07ce605cb475e
walter090 <walter.wu090@gmail.com>
2018-05-13 06:07:52 -0400

    Handle conditional rendering

--- src/components/notification/GenericNotification.js
+++ src/components/notification/GenericNotification.js
@@ -3,13 +3,19 @@ import PropTypes from 'prop-types';
 
 class GenericNotification extends React.Component {
     render() {
+        if (!this.props.notification) {
+            return null;
+        }
+
+        const additionalClass = this.props.additionalClass;
         const notificationList = this.props.notification;
+
         const notificationItems = notificationList.map((notification) =>
             <li key={notification}>{notification}</li>
         );
 
         return (
-            <div className='gen-noti' id={this.props.id}>
+            <div className={'gen-noti ' + (additionalClass ? additionalClass : '')} id={this.props.id}>
                 <ul>{notificationItems}</ul>
             </div>
         );
@@ -20,5 +26,6 @@ export default GenericNotification;
 
 GenericNotification.propTypes = {
     notification: PropTypes.array,
-    id: PropTypes.string
+    id: PropTypes.string,
+    additionalClass: PropTypes.string
 };
