7ce99db2faff3f905df93024721d650852027b66
walter090 <walter.wu090@gmail.com>
2018-05-13 06:09:48 -0400

    Handle notification passed from child

---
+++ src/components/apps/SignUpPage.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import GenericNotification from '../notification/GenericNotification';
+import SignUpForm from '../formContainer/form/SignUpForm';
+
+class SignUpPage extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            notifications: []
+        };
+
+        this.configNotification = this.configNotification.bind(this);
+    }
+
+    configNotification(value) {
+        this.setState(prev => ({
+            notifications: prev.notifications.concat([value])
+        }));
+    }
+
+    render() {
+        return (
+            <div className='form-wrapper'>
+                <GenericNotification notification={this.state.notifications}/>
+                <SignUpForm configNotification={this.configNotification}/>
+            </div>
+        );
+    }
+}
+
+export default SignUpPage;
