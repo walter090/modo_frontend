cdb87ef2a66392228f84b10703077c38baf26a62
walter090 <walter.wu090@gmail.com>
2018-05-28 07:49:58 -0400

    Add card component

---
+++ src/components/listContainer/list/card/Card.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import './Card.css';
+
+class Card extends React.Component {
+    render() {
+        const additionalClass = this.props.additionalClass;
+        return (
+            <div className={'card ' + (additionalClass ? additionalClass : '')}>
+                <div className='card-heading'>
+                    <img src={this.props.imageSource} alt={this.props.cardTitle}/>
+                    <div className='title-block'>
+                        <h1>{this.props.cardTitle}</h1>
+                    </div>
+                </div>
+                <div className='card-desc'>
+                    <p>{this.props.description}</p>
+                    <span className='source'>{this.props.site}</span>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Card;
+
+Card.propTypes = {
+    cardTitle: PropTypes.string,
+    additionalClass: PropTypes.string,
+    imageSource: PropTypes.string,
+    description: PropTypes.string,
+    site: PropTypes.string,
+};
