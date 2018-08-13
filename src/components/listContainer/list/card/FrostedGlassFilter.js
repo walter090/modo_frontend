30bae3eede7f3cb6d19c47f62b7ef7019dcc7704
walter090 <walter.wu090@gmail.com>
2018-08-13 01:53:58 -0400

    Fix extension error

--- src/components/listContainer/list/card/FrostedGlassFilter.js
+++ src/components/listContainer/list/card/FrostedGlassFilter.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-class FrostedGlassFilter extends React {
+class FrostedGlassFilter extends React.Component {
     render() {
         return (
             <svg xmlns="http://www.w3.org/2000/svg" version="1.1">
